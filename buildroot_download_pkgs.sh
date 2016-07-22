8e6a85ca73dcbcc37d101c6e11f6b0fed3fcde29
Joel Carlson <JoelsonCarl@gmail.com>
2016-07-22 00:08:13 -0500

    Add script for buildroot dependency setup
    Adds script that downloads packages for working with buildroot.
    Assumes apt-get.

---
+++ buildroot_download_pkgs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Basic packages
+sudo apt-get install sed make binutils build-essential gcc g++ bash patch gzip bzip2 perl tar cpio python unzip rsync wget
+
+# Configuration interface and source tools
+sudo apt-get install libncurses5 cvs git mercurial rsync subversion
+
+# Documentation and graph generation
+sudo apt-get install asciidoc w3m dblatex graphviz python-matplotlib
