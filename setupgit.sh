3585a03780b3648913e434d29219046244895501
Joel Carlson <JoelsonCarl@gmail.com>
2016-08-13 23:35:14 -0500

    setupgit: add script to configure some git parameters
    Add script to configure git with user name, user email, a commit
    message template, and some other configuration options.
    
    Signed-off-by: Joel Carlson <JoelsonCarl@gmail.com>

---
+++ setupgit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This assumes git is already installed.
+# It configures some git options.
+
+#TODO: add ability to configure name and email from command line
+username="Joel Carlson"
+useremail="JoelsonCarl@gmail.com"
+
+git config --global user.email "${useremail}"
+git config --global user.name "${username}"
+git config --global core.editor vim
+git config --global push.default simple
+git config --global gpg.program gpg2
+
+git_commit_template=~/.git_commit_msg.txt
+echo "subject: summary" > "${git_commit_template}"
+echo >> "${git_commit_template}"
+echo "Description" >> "${git_commit_template}"
+echo >> "${git_commit_template}"
+echo "Signed-off-by: ${username} <${useremail}>" >> "${git_commit_template}"
+git config --global commit.template ~/.git_commit_msg.txt
