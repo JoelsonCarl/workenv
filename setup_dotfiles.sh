ac0b8424fc742b776840f8f405f881af5ba9caec
Joel Carlson <joel.carlson@cosmicaes.com>
2018-04-30 17:18:57 -0600

    tmux: add config file and dotfile linking
    Signed-off-by: Joel Carlson <joel.carlson@cosmicaes.com>

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -15,3 +15,11 @@ if [ $? -eq 0 ]; then
 else
 	echo "vim not found; not linking .vimrc"
 fi
+
+which tmux > /dev/null
+if [ $? -eq 0 ]; then
+	rm -f "${HOME}/.tmux.conf"
+	ln -s "$(pwd)/.tmux.conf" "${HOME}/.tmux.conf"
+else
+	echo "tmux not found; not linking .tmux.conf"
+fi
