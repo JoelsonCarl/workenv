9c26ab646d244ba3754976e5a5b303814922ad56
Joel Carlson <JoelsonCarl@gmail.com>
2020-03-12 21:46:29 -0700

    buildroot_download_pkgs.sh: update for 2020.02 reqs
    Signed-off-by: Joel Carlson <JoelsonCarl@gmail.com>

--- buildroot_download_pkgs.sh
+++ buildroot_download_pkgs.sh
@@ -2,7 +2,7 @@
 
 # Basic packages
 sudo apt-get install sed make binutils build-essential gcc g++ bash patch \
-	gzip bzip2 perl tar cpio python unzip rsync wget
+	gzip bzip2 perl tar cpio unzip rsync file bc wget
 
 # Configuration interface and source tools
 sudo apt-get install libncurses5 libncurses5-dev cvs git mercurial rsync \
