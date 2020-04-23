b5498f5405119214747b37e07567fd5abaa07b7c
Joel Carlson <JoelsonCarl@gmail.com>
2020-04-23 10:32:30 -0700

    buildroot_download_pkgs.sh: update for Ubuntu 20.04, buildroot 2020.02
    Signed-off-by: Joel Carlson <JoelsonCarl@gmail.com>

--- buildroot_download_pkgs.sh
+++ buildroot_download_pkgs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Basic packages
-sudo apt-get install sed make binutils build-essential gcc g++ bash patch \
+sudo apt install sed make binutils build-essential gcc g++ bash patch \
 	gzip bzip2 perl tar cpio unzip rsync file bc wget
 
 # Configuration interface and source tools
-sudo apt-get install libncurses5 libncurses5-dev cvs git mercurial rsync \
+sudo apt install libncurses5 libncurses5-dev cvs git mercurial rsync \
 	subversion
 
 # Documentation and graph generation
-sudo apt-get install asciidoc w3m dblatex graphviz python-matplotlib
+sudo apt install asciidoc w3m dblatex graphviz python3-matplotlib
